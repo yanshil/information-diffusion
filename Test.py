234daab0f5db581f7e9a75a10ac25c343da25970
DongJG <3150103642@zju.edu.cn>
2018-04-14 23:13:32 +0800

    fix conflict in Data.py

--- Test.py
+++ Test.py
@@ -1,24 +1,88 @@
+from Data import Node
+from Model import Model
+
+from typing import List
+import numpy as np
+
+
 class Test(object):
     def __init__(self):
-        self.model = None    # Model*
-        self.diffusionSize = -1    # int*
-        self.diffusionDuration = -1    # int*
+        self.model = None    # type: Model
+        self.diffusionSize = None    # type: List[int]
+        self.diffusionDuration = None    # type: List[int]
 
-    def Simulation(self, Method):    # int method
+    def Simulation(self, method):
+        """
+
+        :param int method:
+        :rtype: int
+        :return:
+        """
+
         pass
-    def SampleRole(self, u):    # int u 
+
+    def SampleRole(self, u):
+        """
+
+        :param int u:
+        :rtype: int
+        :return:
+        """
+        rolenum = self.model.roleNum
+        p = np.ndarray([rolenum])
+        p[0] = self.model.theta[u][0]
+        for r in range(1, rolenum):
+            p[r] = p[r-1] + self.model.theta[u][r]
+        role = np.random.choice(rolenum, size=1, p=p/np.sum(p))
+        return role
+
+    def SampleT(self, r):
+        """
+
+        :param int | Node r:
+        :rtype: int
+        :return: None
+        """
+        if isinstance(r, Node):
+            pass
+        elif isinstance(r, int):
+            t = np.random.geometric(p=self.model)
         pass
-    def SampleT(self, r):    # int r
+
+    def SampleZ(self, r):
+        """
+
+        :param int | Node r:
+        :rtype: int
+        :return: None
+        """
+        if isinstance(r, Node):
+            pass
+        elif isinstance(r, int):
+            pass
         pass
-    def SampleT(self, user):   # arg: Node*
-        pass  # overload?
-    def SampleZ(self, r):   # int r 
+
+    def TrueSize(self, ):
+        """
+
+        :rtype: int
+        :return:
+        """
         pass
-    def SampleZ(self, user):   # Node*
+
+    def TrueDuration(self, ):
+        """
+
+        :rtype: int
+        :return:
+        """
         pass
-    def TrueSize(self, ):    # 
-        pass
-    def TrueDuration(self, ):    # 
-        pass
-    def SizeAndDuration(self, method):    # int method
+
+    def SizeAndDuration(self, method):
+        """
+
+        :param int method:
+        :rtype: int
+        :return:
+        """
         pass
